491f6f8152e5aaf6d0f279b26f812ed16900b50d
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-08-22 21:56:46 +0300

    layout

--- public/app/apps/home/views/mainView.js
+++ public/app/apps/home/views/mainView.js
@@ -1,13 +1,32 @@
 define(function (require) {
 	var Backbone = require('Backbone');
 
+	var HeaderView = require('./headerView');
+	var CompetitorsView = require('./competitorsView');
+	var CounterView = require('./counterView');
+	var FooterView = require('./footerView');
+
 	var MainView = Backbone.View.extend({
 		initialize: function () {
 			this.subviews = [];
 		},
 
 		render: function () {
-			this.$el.append('<h1>MainView</h1>');
+			var header = new HeaderView();
+			this.subviews.push(header);
+			this.$el.append(header.render().el);
+
+			var competitors = new CompetitorsView();
+			this.subviews.push(competitors);
+			this.$el.append(competitors.render().el);
+
+			var counterView = new CounterView();
+			this.subviews.push(counterView);
+			this.$el.append(counterView.render().el);
+
+			var footer = new FooterView();
+			this.subviews.push(footer);
+			this.$el.append(footer.render().el);
 
 			return this;
 		}
